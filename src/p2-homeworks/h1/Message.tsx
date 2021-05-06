670f5a35f134c8eb1872bfc8dc7e1addb2e675e6
Alexander Bunin <81072451+bunin-av@users.noreply.github.com>
2021-05-06 23:06:57 +0300

    Add files via upload
    HW2

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -3,17 +3,17 @@ import {MessageType} from "./HW1";
 import styles from "./Message.module.scss"
 
 
-
-function Message(props:MessageType) {
+function Message(props: MessageType) {
     return (
-        <div className={styles.MessageComponent_wrapper}>
-            <div className={styles.avatar}><img src={props.avatar} alt=""/></div>
-            <div className={styles.message_wrapper}>
-                <div className={styles.userName}>{props.name}</div>
-                <div className={styles.messageText}>{props.message}</div>
-                <div className={styles.messageTime}>{props.time}</div>
-            </div>
-        </div>
+      <div className={styles.MessageComponent_wrapper}>
+          <div className={styles.avatar}><img src={props.avatar} alt="ava"/></div>
+          <div className={styles.angel}></div>
+          <div className={styles.message_wrapper}>
+              <div className={styles.userName}>{props.name}</div>
+              <div className={styles.messageText}>{props.message}</div>
+              <div className={styles.messageTime}>{props.time}</div>
+          </div>
+      </div>
     )
 }
 
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,37 +1,39 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import styles from './Affairs.module.scss'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType>
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
     const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
+      <Affair // should work
+        key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+        affair={a}
+        deleteAffairCallback={props.deleteAffairCallback}
+      />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => props.setFilter('all')
+    const setHigh = () => props.setFilter('high')
+    const setMiddle = () => props.setFilter('middle')
+    const setLow = () => props.setFilter('low')
+
 
     return (
-        <div>
+      <div>
 
-            {mappedAffairs}
+          {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
+          <button onClick={setAll} className={styles.priorityButton}>All</button>
+          <button onClick={setHigh} className={styles.priorityButton}>High</button>
+          <button onClick={setMiddle} className={styles.priorityButton}>Middle</button>
+          <button onClick={setLow} className={styles.priorityButton}>Low</button>
+      </div>
     )
 }
 
